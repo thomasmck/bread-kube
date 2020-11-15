6e354c8a30421f17060f114e7dc035d8318b2214
Thomas Mckelvey <thomasmckelvey92@gmail.com>
2020-11-15 20:35:16 +0000

    Test redis code
    Signed-off-by: Thomas Mckelvey <thomasmckelvey92@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-#!/usr/bin/env python3.7
+#!/usr/bin/env python3.8
 
 from flask import Flask
+from redis import Redis, RedisError
+
+# Connect to Redis
+redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
+    try:
+        visits = redis.incr("counter")
+    except RedisError:
+        visits = "<i>cannot connect to Redis, counter disabled</i>"
+
     return "Hello from Python!"
 
 if __name__ == "__main__":
